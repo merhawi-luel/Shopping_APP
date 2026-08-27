e4723756394401e10288f594e6756c882cc788f5
merhawi-luel <merhawiluel63@gmail.com>
2026-08-27 21:23:15 +0300

    NavBar logic done with reducer and styled as well

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,11 +1,24 @@
 import { Link } from "react-router-dom";
+import { ShoppingCart } from "lucide-react";
+import useCart from "../../hooks/useCart";
+import "../../styles/NavBar.css";
 
 export default function Navbar() {
+  const { cartItems } = useCart();
+ const totalItems = cartItems.reduce((accumulator, currentItem) => {
+  return accumulator + currentItem.quantity;
+}, 0);
+
   return (
-    <nav>
-            <Link to="/">Home  </Link>
-            <Link to="/shop">Shop </Link>
-            <Link to="/cart">Cart  </Link>
+    <nav className="navbar">
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/shop">Shop</Link>
+      </div>
+      <Link to="/cart" className="cart-link">
+        <ShoppingCart size={20} />
+        {totalItems > 0 && <span className="cart-badge">{totalItems}</span>}
+      </Link>
     </nav>
   );
 }
